a083e125b22ce9f9c440b43e108e7bb2b11dcb56
mellecks <scott.mellecker@lumen.com>
2021-08-30 22:37:22 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -152,7 +152,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-     this.connector.get(callback)
+     connector.get(callback)
    }
 
   /**
@@ -171,7 +171,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-    this.connector.post(callback);
+    connector.post(callback);
   }
 }
 
