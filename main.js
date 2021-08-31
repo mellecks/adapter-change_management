33da9d7c426cbad7ff44aa4df018985fbfe7c2e5
mellecks <scott.mellecker@lumen.com>
2021-08-31 19:54:15 +0000

    Lab Final

--- main.js
+++ main.js
@@ -188,10 +188,10 @@ healthcheck(callback) {
    * Note how the object was instantiated in the constructor().
    * get() takes a callback function.
    */
-    this.connector.get((data,error) => { callback(data,error)
-      console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
-    });
-  }
+    this.connector.get((data,error) => { callback(data,error); 
+        console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
+     });
+  }  
 
   /**
    * @memberof ServiceNowAdapter
