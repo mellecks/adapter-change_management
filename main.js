b45432f60a14b38ed871e7857bddfbf545c1a32c
mellecks <scott.mellecker@lumen.com>
2021-08-31 16:01:37 +0000

    Lab 6

--- main.js
+++ main.js
@@ -115,8 +115,9 @@ healthcheck(callback) {
        * for the callback's errorMessage parameter.
        */
       this.emitOffline(error); 
-    } else if (result.body.includes('Instance Hibernating page')) {
+    } else if (this.connector.is.Hibernating(response)) {
       error = 'Service Now instance is hibernating';
+      this.emitOffline(error); 
     } else { 
       /**
        * Write this block.
